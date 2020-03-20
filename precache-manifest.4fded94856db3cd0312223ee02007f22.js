1d3fb85568654ca46f241c31f62246672dfec052
Mohammed H. AlMuhanna <almuhannamh@outlook.com>
2020-03-20 13:15:47 +0300

    Updates

---
+++ precache-manifest.4fded94856db3cd0312223ee02007f22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65b52d10bbbc472ccac2f4deb6d965e8",
+    "url": "/ppp-frontend/index.html"
+  },
+  {
+    "revision": "bbb8c112f2e985e4eb83",
+    "url": "/ppp-frontend/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "cd705af25f49f23ff385",
+    "url": "/ppp-frontend/static/css/main.36a53ea7.chunk.css"
+  },
+  {
+    "revision": "bbb8c112f2e985e4eb83",
+    "url": "/ppp-frontend/static/js/2.c48405f4.chunk.js"
+  },
+  {
+    "revision": "9a0e6ad7df4effc3aa6ea7f039958117",
+    "url": "/ppp-frontend/static/js/2.c48405f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd705af25f49f23ff385",
+    "url": "/ppp-frontend/static/js/main.e929bdae.chunk.js"
+  },
+  {
+    "revision": "ea01f0db0008893a6ae7",
+    "url": "/ppp-frontend/static/js/runtime-main.c785877e.js"
+  }
+]);
